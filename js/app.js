e24277a58394e7f2f3a82c2072fab6a9c952b528
GULLAPALLIBABJICHANDRASEKHAR <hemalathasamudrala20@gmail.com>
2025-03-05 20:42:56 +0530

    Responsive Design added

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ function sideBarDisplay() {
     const sideBarContainer = document.querySelector(".sidebar");
     const hamburgerBtn = document.querySelector(".hamburger");
     const hamburger_lines = document.querySelectorAll(".line");
+    const mainContainer = document.querySelector(".bento-container");
+    console.log(mainContainer);
+    
     hamburgerBtn.addEventListener("click", ()=> {
         const sideBarClasses = sideBarContainer.classList;
         if (sideBarClasses.contains("show-sidebar")) {
@@ -9,12 +12,16 @@ function sideBarDisplay() {
                 hamburger_lines[i].classList.remove("line-clicked");
             }
             sideBarContainer.classList.remove("show-sidebar");
+            mainContainer.classList.remove("bento-container-move");
+            mainContainer.classList.add("bento-container");
         }
         else {
             for(i=0; i<hamburger_lines.length; i++) {
                 hamburger_lines[i].classList.add("line-clicked");
             }
             sideBarContainer.classList.add("show-sidebar");
+            mainContainer.classList.remove("bento-container");
+            mainContainer.classList.add("bento-container-move");
         }
     });
 }
